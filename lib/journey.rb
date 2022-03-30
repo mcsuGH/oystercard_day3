0f449e7f2b6265019c8b27bed48b20e24b7f67b3
Jordan <jordanmanu@live.com.au>
2022-03-30 15:08:39 +0100

    refactored journey and made tests pass

--- lib/journey.rb
+++ lib/journey.rb
@@ -2,11 +2,18 @@ class Journey
 
 attr_accessor :exit_station, :journey, :entry_station, :current_journey, :journey_history
 
+MAXIMUM_FARE = 6
+MINIMUM_FARE = 1
+
     def initialize(entry_station)
-        @current_journey = {entry_station: nil, exit_station: nil}
-        @entry_station = entry_station
+        record_journey(entry_station)
         @exit_station = nil
         @journey_history = []
+    end
+
+    def record_journey(entry_station)
+        @entry_station = entry_station
+        @current_journey = {entry_station: nil, exit_station: nil}
         @current_journey[:entry_station] = entry_station
     end
 
@@ -14,13 +21,19 @@ attr_accessor :exit_station, :journey, :entry_station, :current_journey, :journe
         @exit_station = exit_station
         @current_journey[:exit_station] = exit_station
         @journey_history.push(@current_journey)
+        @entry_station = nil
+        self
     end
 
-    def in_journey?
-        if @entry_station != nil
+    def complete?
+        if @entry_station == nil
           return true
+        else
+            false
         end
     end
 
-
+    def fare
+    complete? ? MINIMUM_FARE : MAXIMUM_FARE
+    end
 end
--- spec/journey_spec.rb
+++ spec/journey_spec.rb
@@ -2,9 +2,26 @@ require_relative '../lib/journey.rb'
 
 describe Journey do
     let(:entry_station) { double :entry_station}
+    let(:journey) { Journey.new(entry_station)}
+    let(:exit_station) { double :exit_station }
+
     it "should remember the entry station after touch in" do
         journey = Journey.new(entry_station)
         journey.entry_station
         expect(journey.entry_station).to eq entry_station
+    end
+    it "Should deduct a max limit fare if user touches in but doesn't touch out " do
+        expect(journey.fare).to eq Journey::MAXIMUM_FARE
+    end
+    it "Should deduct a minimum fare of 1 if user touches in and touches out" do
+        journey.end_journey(exit_station)
+        expect(journey.fare).to eq 1
+    end
+    it "returns itself when exiting a journey" do
+        expect(journey.end_journey(exit_station)).to eq(journey)
+    end
+    it "knows if a journey is complete" do
+        journey.end_journey(exit_station)
+        expect(journey.complete?).to eq true
     end
 end
